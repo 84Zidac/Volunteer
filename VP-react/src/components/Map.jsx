1a22ff23f4e7efcc4945f365dfe52b7652dc511c
Marcus Gibson <114510052+84Zidac@users.noreply.github.com>
2023-05-04 09:06:38 -0800

    map-006: Added search function to adjust map, added details to map markers, removed dummy data from HomePage.jsx

--- VP-react/src/components/Map.jsx
+++ VP-react/src/components/Map.jsx
@@ -3,32 +3,55 @@ import React from "react";
 import { GoogleMap, Marker, InfoWindow, Circle } from "@react-google-maps/api";
 
 // Define the Map component and receive the addresses prop
-export default function Map({ addresses }) {
+export default function Map({ events }) {
   // Declare state variables to store activeMarker, userLocation, and circleDistance
   const [activeMarker, setActiveMarker] = React.useState(null);
   const [userLocation, setUserLocation] = React.useState(null);
   const [circleDistance, setCircleDistance] = React.useState(5);
+  const [zipCode, setZipCode] = React.useState(null);
+
+
+    // Define a function to geocode the zip code and set the userLocation state variable
+    function geocodeZipCode() {
+      console.log(`Zipcode: ${JSON.stringify(zipCode)}`);
+      const geocoder = new window.google.maps.Geocoder();
+      geocoder.geocode({ address: JSON.stringify(zipCode) }, (results, status) => {
+        if (status === "OK") {
+          setUserLocation({
+            lat: results[0].geometry.location.lat(),
+            lng: results[0].geometry.location.lng(),
+          });
+        } else {
+          console.error(`Geocode failed for ${zipCode} with status ${status}`);
+        }
+      });
+    }
+    function getUserLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            setUserLocation({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            });
+          },
+          (error) => {
+            console.error(`Geolocation error: ${error.message}`);
+            setUserLocation({ lat: 37.2390, lng: -115.8121 }); // Set default location to Area 51
+          }
+        );
+      } else {
+        console.error("Geolocation is not supported by this browser.");
+        setUserLocation({ lat: 37.2390, lng: -115.8121 }); // Set default location to Area 51
+      }
+    }
 
   // Use the useEffect hook to get the user's location
   React.useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setUserLocation({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          });
-        },
-        (error) => {
-          console.error(`Geolocation error: ${error.message}`);
-          setUserLocation({ lat: 41.8781, lng: -87.6298 }); // Set default location to Chicago
-        }
-      );
-    } else {
-      console.error("Geolocation is not supported by this browser.");
-      setUserLocation({ lat: 41.8781, lng: -87.6298 }); // Set default location to Chicago
-    }
+    getUserLocation()
   }, []);
+
+  
 
   // Define a function to handle the marker click event
   function handleMarkerClick(marker) {
@@ -51,12 +74,19 @@ export default function Map({ addresses }) {
 
   // If the userLocation state variable is not set, display a message asking the user to enable geolocation
   if (!userLocation) {
-    return <div>If map fails to load, please enable geolocation</div>;
+    return <div>Loading Map...</div>;
   }
 
   // Return the GoogleMap component with the necessary properties and children components
   return (
     <div id="googleMap">
+      <div><button onClick={getUserLocation}>My Location</button></div>
+<div className="zipcode-form">
+  {/* Display a label and input field to allow the user to enter a zip code */}
+  <label htmlFor="zipCode">Search Location:</label>
+  <input type="text" id="zipCode" name="zipCode" onChange={(e) => setZipCode(e.target.value)} />
+  <button onClick={geocodeZipCode}>Submit</button>
+</div>
       <div className="distance-select">
         {/* Display a label and select box to allow the user to choose the circle distance */}
         <label htmlFor="distance">Circle Distance:</label>
@@ -94,14 +124,15 @@ export default function Map({ addresses }) {
         )}
 
         {/* Render a marker for each address in the addresses prop */}
-        {addresses.map((address, index) => (
+        {events && events.map((event, index) => (
           <Marker
             key={index}
             position={null}
-            label={`${index + 1}`}
+            label={`${event.event_name}`}
             onLoad={(marker) => {
               // Geocode the address to get its latitude and longitude
               const geocoder = new window.google.maps.Geocoder();
+              let address = `${event.address.street}, ${event.address.city}, ${event.address.state} ${event.address.zip}`
               geocoder.geocode({ address }, (results, status) => {
                 if (status === "OK") {
                   marker.setPosition(results[0].geometry.location);
@@ -117,7 +148,12 @@ export default function Map({ addresses }) {
             {/* Display an InfoWindow for the active marker */}
             {activeMarker === index && (
               <InfoWindow onCloseClick={handleInfoWindowClose}>
-                <div>{address}</div>
+                <>
+                <div className="eventDetails"><b>{`${event.organization}`}</b></div>
+                <div className="eventDetails"> {`${event.description}`} </div>
+                <div className="eventDetails">{`When: ${event.start_time}`}</div>
+                <div className="eventDetails">{`${event.address.street}, ${event.address.city}, ${event.address.state} ${event.address.zip}`}</div>
+                </>
               </InfoWindow>
             )}
           </Marker>
