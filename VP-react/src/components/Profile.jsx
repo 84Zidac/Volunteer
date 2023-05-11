4239bc10dc58601be7ccfc0403804d6dd1de41f1
LReva <lenareva338@gmail.com>
2023-05-10 22:50:07 -0400

    profile.jsx and profile.css with additional functions added

--- VP-react/src/components/Profile.jsx
+++ VP-react/src/components/Profile.jsx
@@ -1,58 +1,200 @@
-import { useContext} from "react";
+import { useContext, useState} from "react";
 import {UserContext, OrgContext} from "../App";
-import {Typography} from '@mui/material';
 import "./Profile.css"
+import { editProfile, verifyPassword, saveAboutMe} from "../utilities";
+import volunteer from '../images/volunteer.jpg'
+import { Edit, Save } from '@mui/icons-material';
+import {Box, Dialog, DialogTitle, DialogContent, DialogActions, IconButton, Typography, TextField, Alert } from '@mui/material';
 
 
 export default function Profile(){
-  const {user} = useContext(UserContext)
+  const {user, setUser} = useContext(UserContext)
   const {organization} = useContext(OrgContext)
+  const [editMode, setEditMode] = useState(false);
+  const [oldPassword, setOldPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [newConfirmedPassword, setNewConfirmedPassword] = useState('');
+  const [passwordError, setPasswordError] = useState(false);
+  const [newPasswordError, setNewPasswordError] = useState(false);
+  const [aboutMe, setAboutMe] = useState(user.aboutMe)
+  const [changeSuccess, setChangeSuccess] = useState(false)
+  const [editDisabled, setEditDisabled] = useState(true)
+  const [save, setSave] = useState(false)
 
   const capitalized = (name) =>{
       let nameLower = name.toLowerCase().split(' ') 
        return nameLower.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
   }
   
+  const openChangePasswordDialog = () => {
+    setEditMode(true)
+  }
+
+  const handleCancelEdit = () => {
+    setEditMode(false);
+    setOldPassword('');
+    setNewPassword('');
+    setNewConfirmedPassword('')
+
+  };
+
+  const handleChangePassword = async (event) => {
+    event.preventDefault();
+    let response = await verifyPassword(oldPassword)
+    if (response.password === false) {
+      setPasswordError(true);
+      setOldPassword('');
+      setNewPassword('');
+      setNewConfirmedPassword('')
+    } else {
+      if (newPassword !== newConfirmedPassword) {
+        setNewPasswordError(true);
+        setOldPassword('');
+        setNewPassword('');
+        setNewConfirmedPassword('')
+      } else {
+        editProfile(newPassword);
+        setNewConfirmedPassword('');
+        setNewPassword('');
+        setOldPassword("")
+        setChangeSuccess(true);
+      }
+    }
+  }
+
+  const handleEditButtonClick = () =>{
+    setEditDisabled(false)
+    setSave(false)
+  }
+
+  const handleSaveButtonClick = async(event) =>{
+    event.preventDefault();
+    setSave(true)
+    setEditDisabled(true)
+    saveAboutMe(aboutMe, setUser)
+  }
+
+  
   return (
-            <div className="profile-div">
-              <Typography variant="h6" gutterBottom>
-                Full Name: {capitalized(user.name)}
-              </Typography>
-              <Typography variant="h6" gutterBottom>
-                Email: {user.email}
-              </Typography>
-              { user.isCoordinator ? (<div className="profile-subdiv">
-                <hr></hr>
-                <Typography variant="h4" gutterBottom>
-                Organization Details
-                </Typography>
-                <Typography variant="h6" gutterBottom>
-                  Name: {organization.organization_name}
-                </Typography>
-                <Typography variant="h6" gutterBottom>
-                  Description: {organization.description}
-                </Typography>
-                <Typography variant="h6" gutterBottom>
-                  Website: {organization.website}
-                </Typography>
-                <Typography variant="h6" gutterBottom>
-                  Phone number:{organization.phone_number}
-                </Typography>              
-                <Typography variant="h6" gutterBottom>
-                  Email: {organization.contact_email}
-                </Typography>
+          <div className="profile-main">
+            <Box
+              sx={{
+                width: 600,
+                height: 600,
+                backgroundColor: '#84A9AC',
+                pt: 2, pr: 3, pb: 4, pl: 5,
+                borderRadius: '10%',
+              }}>
+                <div className="profile-div">
+                  <Typography variant="h4" gutterBottom>
+                    <u>My Profile</u>
+                    </Typography>
+                  <Typography variant="h6" gutterBottom>
+                    Full Name: {capitalized(user.name)}
+                  </Typography>
+                  <Typography variant="h6" gutterBottom>
+                    Email: {user.email}
+                  </Typography>
+                  { user.isCoordinator ? (<div className="profile-subdiv">
+                    <hr></hr>
+                    <Typography variant="h4" gutterBottom>
+                    <u>Organization Details</u>
+                    </Typography>
+                    <Typography variant="h6" gutterBottom>
+                      Name: {organization.organization_name}
+                    </Typography>
+                    <Typography variant="h6" gutterBottom>
+                      Description: {organization.description}
+                    </Typography>
+                    <Typography variant="h6" gutterBottom>
+                      Website: {organization.website}
+                    </Typography>
+                    <Typography variant="h6" gutterBottom>
+                      Phone number:{organization.phone_number}
+                    </Typography>              
+                    <Typography variant="h6" gutterBottom>
+                      Email: {organization.contact_email}
+                    </Typography>
+                    </div>
+                  ): (
+                    <div className="profile-subdiv">
+                  <Typography variant="h6" gutterBottom>
+                    Events attended: 0
+                  </Typography>
+                  <Typography variant="h6" gutterBottom>
+                    About me:
+                  </Typography>
+                  <TextField
+                    value={aboutMe}
+                    variant="outlined"
+                    multiline
+                    maxRows={10}
+                    contentEditable={true}
+                    disabled = {editDisabled}
+                    style = {{width: '500px'}}
+                    inputProps={{ maxLength: 500}}
+                    onChange={(e)=> {setAboutMe(e.target.value)}}
+                    InputProps={{
+                      style: {
+                        color: '#204051',
+                        borderColor: '#204051',
+                      },
+                      endAdornment:(
+                        <div>
+                          {editDisabled ? (
+                            <IconButton onClick={handleEditButtonClick}>
+                              <Edit />
+                            </IconButton>
+                          ): ( <IconButton onClick={handleSaveButtonClick}>
+                            <Save />
+                          </IconButton>)}
+                        </div>)              
+                    }}
+                  />
+                  </div>
+                  )} 
+                    {save  && (
+                      <Alert severity="success" onClose={() => setSave(false)}>
+                        Saved!
+                      </Alert> )}
+                  <button onClick={openChangePasswordDialog} className='change-button'>
+                  Change Password
+                </button>
+                <Dialog open={editMode} onClose={handleCancelEdit}>
+                  <DialogTitle>Change Password</DialogTitle>
+                  <form onSubmit={handleChangePassword}>
+                    <DialogContent>
+                      <TextField label="Current Password" fullWidth value={oldPassword} type="password" onChange={(e) => setOldPassword(e.target.value)} />
+                    </DialogContent>
+                    <DialogContent>
+                      <TextField label="New Password" fullWidth value={newPassword} type="password" onChange={(e) => setNewPassword(e.target.value)} />
+                    </DialogContent>
+                    <DialogContent>
+                      <TextField label="Confirm New Password" fullWidth value={newConfirmedPassword} type="password" onChange={(e) => setNewConfirmedPassword(e.target.value)} />
+                    </DialogContent>
+                    <DialogActions>
+                      <button className='change-button'>Change</button>
+                    </DialogActions>
+                  </form>
+                  {passwordError && (
+                  <Alert severity="error" onClose={() => setPasswordError(false)}>
+                    Your old password is incorrect. Try again!
+                  </Alert> )}
+                  {newPasswordError && (
+                  <Alert severity="error" onClose={() => setNewPasswordError(false)}>
+                    Your new passwords didn't match. Try again!
+                  </Alert> )}
+                  {changeSuccess && (
+                  <Alert severity="success" onClose={() => setChangeSuccess(false)}>
+                    Your password was changed!
+                  </Alert> )}
+                </Dialog>
                 </div>
-              ): (
-                <div className="profile-subdiv">
-              <Typography variant="h6" gutterBottom>
-                Events attended:
-              </Typography>
-              <Typography variant="h6" gutterBottom>
-                About me:
-              </Typography>
+              </Box>
+              <div className="profile-image">
+                <img src={volunteer} alt="volunteers image" style={{ width: '600px', height: '600px', borderRadius: '50%' }}/>
+                <a href="https://www.freepik.com/free-vector/people-volunteering-donating-money-items_3530067.htm#query=volunteer&position=4&from_view=search&track=sph" style={{ fontSize: '10px', color: '#204051' }}>Image by rawpixel.com on Freepik</a>
               </div>
-               )
-              } 
             </div>
   )
 }
